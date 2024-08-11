5022ed79dd4aba4c86ecd2bb765a70b8e9fb35b1
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-11 08:39:39 +0530

    Minor changes

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 from flask_cors import CORS
 
 from backend.routes.chat import bp_chat
-from backend.handlers.before_request import before_request
 
 
 # -----------------------------------------------------------------------------
@@ -15,7 +14,28 @@ CORS(bp_app, resources={r"/*": {"origins": "*"}})
 # Register before request handler for authentication
 # -----------------------------------------------------------------------------
 
-bp_app.before_request(before_request)
+
+@bp_app.before_request
+def before_request():
+    # setattr(request, "userID", "aqE2MxBnijRk7cZbP3lQj63RIyF2")
+
+    return None
+
+    if request.endpoint == "health":
+        return None
+
+    id_token = request.headers.get("Authorization-Token")
+    if not id_token:
+        return jsonify({"message": "Unauthorized"}), 401
+
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        setattr(request, "userID", decoded_token)
+    except Exception as e:
+        print("Token verification failed:", e)
+        return jsonify({"error": "Invalid ID token"}), 401
+
+    return None
 
 
 # -----------------------------------------------------------------------------
